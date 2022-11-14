931baa11ffbcb29d4d084a694d7f330a8dcc4764
Graeme Tank <graeme.tank@gmail.com>
2022-11-14 00:27:56 -0500

    markers for 5 cities added to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -46,8 +46,8 @@ let cities = [{
   // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
+    L.marker(city.location).addTo(map);
 });
-
 
 /*
 // Make a 100m radius circle on LA
